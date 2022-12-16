b8a7999b5e1f1b4cc2efacbbe10b42a92320204e
janrth <jan_rathfelder@yahoo.de>
2022-12-16 22:38:08 +0100

    add all relevant files

---
+++ chat.py
@@ -0,0 +1,74 @@
+import random
+import json
+import pickle
+import torch
+
+from linear_nn import NeuralNet
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+#############################
+
+# activate stemmer and device for pytroch:
+stemmer = PorterStemmer()
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# load json file with training data:
+json_file_path = "intents.json"
+with open(json_file_path, 'r') as j:
+     contents = json.loads(j.read())
+
+FILE = "data.pth"
+data = torch.load(FILE)
+
+# define words to ignore when tokenize and stemming:
+ignore_words = ['?', '.', '!', ',']
+
+# Find input size, size of hidden layers and output size + tags:
+input_size = data["input_size"]
+hidden_size = data["hidden_size"]
+output_size = data["output_size"]
+tags = data['tags']
+model_state = data["model_state"]
+
+# Instantiate pre-trained pytorch model:
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model.load_state_dict(model_state)
+model.eval()
+
+# Load fitted vectorization for encoding:
+vectorization_encoder = pickle.load(open('vectorization_encoder', 'rb'))
+
+# Define bot name:
+bot_name = "Yara"
+
+# Start chat:
+print("Let's chat! (type 'quit' to exit)")
+while True:
+    sentence = input("You: ")
+    if sentence == "quit":
+        break
+    # stem + tokenize for input, before detokenization
+    sentence = TreebankWordDetokenizer().detokenize([stemmer.stem(word) for word in 
+                                                nltk.word_tokenize(sentence) if word not in ignore_words])
+    sentence = [sentence] # input sentence needs to be a list for vectorization
+    X = vectorization_encoder.transform(sentence) # apply pre-fitted vectorization
+    X = torch.from_numpy(X.toarray()).to(device) # from numpy to torch 
+
+    output = model(X.float()) # make predict
+    _, predicted = torch.max(output, dim=1) # return prediction with highest cross-entropy
+
+    tag = tags[predicted.item()] # find correct tag
+
+    probs = torch.softmax(output, dim=1) # find cross-entropy from softmax layer
+    prob = probs[0][predicted.item()]  
+    
+    # If cross-entropy prob is big enough (so the bot is confident enough), then return an answer,
+    # otherwise reply to repeat the question:
+    if prob.item() > 0.6:
+        for intent in contents['intents']:
+            if tag == intent["tag"]:
+                print(f"{bot_name}: {random.choice(intent['responses'])}")
+    else:
+        print(f"{bot_name}: I do not understand, could you rephrase your answer, please. If we can not help you here, please visit our FAQ page at -insert_your_link_here-")
---
+++ train_chatbot.py
@@ -0,0 +1,116 @@
+import numpy as np
+import nltk 
+import torch
+import torch.nn as nn
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+from torch.utils.data import Dataset, DataLoader
+import json
+import pickle
+
+import data_set
+import linear_nn
+
+#nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+##########################
+
+def detokenize_fct(x):
+    '''
+    Applies detokanization to obtain full sentences.
+    '''
+    return TreebankWordDetokenizer().detokenize(x)
+
+# Instantiate stemmer and count-vectorizer:
+stemmer = PorterStemmer()
+vectorizer = CountVectorizer()
+
+# load json file with training data:
+json_file_path = "intents.json"
+with open(json_file_path, 'r') as j:
+     contents = json.loads(j.read())
+
+
+# Training begins here
+# Create empty lists to save tags and full sentences + define signs to be ignored 
+# for stemming and tokenization:
+tags = []
+sentences = []
+
+# loop through each sentence in training data:
+for intent in contents['intents']:
+    tag = intent['tag']
+    for pattern in intent['patterns']:
+        # tokenization and stemming applied to each word, before detokenized sentence is saved
+        sentences.append(detokenize_fct([stemmer.stem(word) for word in nltk.word_tokenize(pattern)]))
+        tags.append(tag)
+
+# The detokanization is needed as the vectorizer is applied on one list of many sentences:
+X_train = vectorizer.fit_transform(sentences)
+
+# Create unique tags and numeric class labels: 
+tags_unique = sorted(set(tags)) 
+y_train = [tags_unique.index(i) for i in tags] 
+
+# save vectorizer:
+pickle.dump(vectorizer, open('vectorization_encoder', 'wb'))
+
+# Train model:
+X_train = X_train.toarray()
+y_train = np.array(y_train)
+
+# Hyper-parameters for pytroch NeuralNet: 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+num_epochs = 500
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
+
+# Create dataset to be consumed by the model + instantiate the NeuralNetwork:
+dataset = data_set.ChatDataset(X_train, y_train)
+model = linear_nn.NeuralNet(input_size, hidden_size, output_size).to(device)
+
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=2)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(device)
+        
+        # Forward pass
+        outputs = model(words.float())
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+print(f'final loss: {loss.item():.4f}')
+
+# Save the model:
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"tags": tags_unique
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'training complete. file saved to {FILE}')
